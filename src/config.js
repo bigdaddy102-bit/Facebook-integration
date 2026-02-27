a6b3f9d2bc0f97844a2084641fd63ae37e87e2e9
David Hejduk <bigdaddy102@web.de>
2026-02-27 03:28:11 +0100

    Created src/config.js file with Facebook configuration.

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  appId: process.env.FACEBOOK_APP_ID,
+  appSecret: process.env.FACEBOOK_APP_SECRET,
+  pageAccessToken: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
+  apiVersion: 'v18.0',
+  baseUrl: 'https://graph.facebook.com'
+};
